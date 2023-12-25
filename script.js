bc2399415fcf4e0f8fb11d4ad7678dd863f6610e
Aafreen Parveen <143151296+Aafreen18@users.noreply.github.com>
2023-12-25 17:08:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,63 @@
-window.onload = function () {
+window.onload = displayed;
+function displayed() {
     var cells = document.getElementsByClassName("cell");
-
+    
     for (var i = 0; i < cells.length; i++) {
         cells[i].querySelector("i").className = "fa-regular fa-square-full";
     }
 }
 
+function resetGame(){
+    gameOver = false;
+    Player_Turn = false;
+    draw = false;
+    var content = document.querySelector(".content");
+    content.style.display="flex";
+    p.innerHTML = "";
+    Btn.innerHTML = "";
+    Btn.style.visibility="hidden";
+    message.style.display="none";
+
+}
+
 var Player_Turn = false;
 var draw = false;
-var gameOver = false;
+var gameOver=false;
+var cells = document.getElementsByClassName("cell");
+var p = document.querySelector("p");
+var Btn = document.querySelector("button");
+var content = document.querySelector(".content");
+var message = document.querySelector(".message");
+message.style.display="none";
 function printWinner(count1,count2)
 {
-    let p = document.querySelector("p");
     if (count1 == 3) {
         console.log("player1Won");
         gameOver = true;
+        message.style.height="50%";
+        message.style.display="block";
         p.innerHTML = "Player 1 won!";
+        Btn.style.visibility="visible";
+        Btn.innerHTML = "Play Again";
+        displayed();
+        content.style.display="none";
+        Btn.addEventListener("click",resetGame);
     }
     else if (count2 == 3) {
         console.log("player2Won");
         gameOver = true;
+        message.style.height="50%";
+        message.style.display="block";
         p.innerHTML = "Player 2 won!";
+        Btn.style.visibility="visible";
+        Btn.innerHTML = "Play Again";
+        displayed();
+        content.style.display="none";
+        Btn.addEventListener("click",resetGame);
     }
+    
 }
 function checkDraw() {
-    var cells = document.getElementsByClassName("cell");
-    let p = document.querySelector("p");
     draw=true;
     for (var i = 0; i < cells.length; i++) {
         if (cells[i].querySelector("i").getAttribute("class") == "fa-regular fa-square-full") {
@@ -34,8 +66,15 @@ function checkDraw() {
     }
     if (draw) {
         gameOver = true;
+        message.style.height="50%";
+        message.style.display="block";
         console.log("draw");
         p.innerHTML = "Draw!";
+        Btn.style.visibility="visible";
+        Btn.innerHTML = "Play Again";
+        displayed();
+        content.style.display="none";
+        Btn.addEventListener("click",resetGame);
     }
     
 }
@@ -72,8 +111,6 @@ function checkOver() {
         count1=0;
         count2=0;
     }
-    
-
     for (let j = 0; j < 3; j++) {
         var row = document.getElementsByClassName("r-"+j);
         for (var i = 0; i < row.length; i++){
@@ -83,11 +120,8 @@ function checkOver() {
             else if (row[i].querySelector("i").getAttribute("class") == "fa-solid fa-xmark" && i==j){
                 count2++;
             }
-            
-            
         }
         printWinner(count1,count2);
-    
     }
     count1=0;
     count2=0;
@@ -101,15 +135,11 @@ function checkOver() {
             else if (row[i].querySelector("i").getAttribute("class") == "fa-solid fa-xmark" && row.length-1-i==j){
                 count2++;
             }
-            
-            
         }
         printWinner(count1,count2);
-    
     }
     count1=0;
     count2=0;
-
 
     if (!gameOver) { checkDraw(); }
 }
@@ -142,9 +172,6 @@ function Put(obj) {
         console.log(icon, icon.className);
     }
 
-    
     console.log(setter_value, obj);
-
     checkOver();
-
 }
